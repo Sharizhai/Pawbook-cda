88416cb6d1d1155c6c0de42b23ce9a1874a909a2
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-09-21 10:44:08 +0200

    Update env.d.ts

--- packages/server/src/types/env.d.ts
+++ packages/server/src/types/env.d.ts
@@ -1,6 +1,29 @@
 export interface IEnv {
     PORT: number;
     NODE_ENV: "development" | "production" | "test";
-    
+
+    JWT_SECRET: string;
+    JWT_EXPIRATION_SECRET: string;
+    REFRESH_TOKEN_SECRET: string;
+    REFRESH_TOKEN_EXPIRATION_SECRET: string;
+
+    FRONTEND_URL: string;
     ORIGIN: string;
+    MONGO_URI: string;
+
+    // Nodemailer
+    EMAIL_USER: string;
+    EMAIL_APP_PASSWORD: string;
+    JWT_RESET_PWD_SECRET: string;
+
+    // Cloudinary
+    CLOUDINARY_URL: string;
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
+    LOGO_URL: string;
+
+    // Helmet
+    FRONTEND_DOMAIN: string;
+    BACKEND_URL: string;
 }
