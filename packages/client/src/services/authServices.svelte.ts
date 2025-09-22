cb4fc0f67476f02f928f235d26ae1cf1601ed030
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-09-22 18:04:01 +0200

    create backendServices & create an apiFetch function + add content in authLogin & authLogout functions in authServices

--- packages/client/src/services/authServices.svelte.ts
+++ packages/client/src/services/authServices.svelte.ts
@@ -1,7 +1,29 @@
+import { apiFetch } from "./backendServices";
+import {user} from "$stores/stores.svelte";
+import {push} from "svelte-spa-router";
+
 export async function authLogin(email: string, password: string) {
-    //TODO
+    const response = await apiFetch("/auth/login", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        checkCredentials: false,
+    })
+
+    const result = await response.json();
+
+    if(!response.ok) {
+        console.error(result.errorCode);
+
+        return false;
+    }
+
+    user.setAccessToken(result.data.token);
+    return true;
 }
 
 export async function authLogout() {
-    //TODO
+    user.setAccessToken("");
+    user.clearInformations();
+
+    push("/");
 }
---
+++ packages/client/src/services/backendServices.ts
@@ -0,0 +1,50 @@
+import { authLogout } from '$services/authServices.svelte';
+import { user } from '$stores/stores.svelte';
+import type { APIFetchRequestInit } from '$types/backendTypes';
+import { isTokenExpired } from '$utils/tokenUtils';
+
+export const backendURL = `${import.meta.env.VITE_PUBLIC_BACKEND_URL}/api`;
+
+export async function apiFetch(
+    url: string,
+    init?: APIFetchRequestInit,
+): Promise<Response> {
+    const headers = init?.headers ?? new Headers();
+
+    if (init?.checkCredentials ?? true) {
+        if (!(await validateUserCredentials())) {
+            await authLogout();
+
+            return Promise.reject(new Error('Invalid credentials'));
+        }
+
+        const accessToken = user.accessToken;
+        headers.append('Authorization', `Bearer ${accessToken}`);
+    }
+
+    if (!headers.has('Content-Type'))
+        headers.append('Content-Type', 'application/json');
+
+    try {
+        const response = await fetch(`${backendURL}${url}`, {
+            method: init?.method ?? 'GET',
+            body: init?.body ?? null,
+            headers,
+            credentials: 'include',
+            signal: init?.controller?.signal ?? null,
+        });
+
+        return response;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+}
+
+export async function validateUserCredentials(): Promise<boolean> {
+    const accessToken = $state.snapshot(user.accessToken);
+
+    if (accessToken && !isTokenExpired(accessToken)) return true;
+
+    return false;
+}
