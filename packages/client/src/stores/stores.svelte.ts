0ede83a45278f7c655fd9ce26f838d272bef3cca
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-09-22 18:04:45 +0200

    create userStore and add it to stores

--- packages/client/src/stores/stores.svelte.ts
+++ packages/client/src/stores/stores.svelte.ts
@@ -1,3 +1,6 @@
+import {createUserSlice} from "./userStore.svelte";
 import { createAppSlice } from "./appStore.svelte";
 
 export const app = createAppSlice();
+
+export const user = createUserSlice();
---
+++ packages/client/src/stores/userStore.svelte.ts
@@ -0,0 +1,37 @@
+import { type UserInformations} from "$types/userTypes";
+import {user} from "$stores/stores.svelte";
+
+export function createUserSlice() {
+    let information: UserInformations = $state({});
+    let accessToken: string = $state(localStorage.getItem("accessToken") ?? "");
+
+    let isAdmin: boolean = $derived(user.information.role === "admin");
+
+    function setAccessToken(token: string) {
+        accessToken = token;
+        localStorage.setItem("accessToken", token);
+    }
+
+    function clearInformations() {
+        information = {};
+    }
+
+    return {
+        get information() {
+            return information;
+        },
+        get isAdmin() {
+            return isAdmin;
+        },
+        get accessToken() {
+            return accessToken;
+        },
+
+        set information(value: UserInformations) {
+            information = value;
+        },
+
+        clearInformations,
+        setAccessToken,
+    }
+}
