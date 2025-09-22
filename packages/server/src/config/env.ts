0bbdfc83a146dabb9870ff851986b5b80ce32771
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-09-22 18:08:16 +0200

    update env.d.ts & env.ts

--- packages/server/src/config/env.ts
+++ packages/server/src/config/env.ts
@@ -1,4 +1,4 @@
-import { IEnv } from "../types/env";
+import { IEnv } from "$types/env";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -7,7 +7,26 @@ export const env: IEnv = {
     PORT: parseInt(process.env.PORT || "3000"),
     NODE_ENV: process.env.NODE_ENV as 'development' | 'production' | 'test',
 
+    JWT_SECRET: process.env.JWT_SECRET || (() => {
+        throw new Error('JWT_SECRET must be defined');
+    })(),
+    JWT_EXPIRATION_SECRET: process.env.JWT_EXPIRATION_SECRET || (() => {
+        throw new Error('JWT_EXPIRATION_SECRET must be defined');
+    })(),
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || (() => {
+        throw new Error('REFRESH_TOKEN_SECRET must be defined');
+    })(),
+    REFRESH_TOKEN_EXPIRATION_SECRET: process.env.REFRESH_TOKEN_EXPIRATION_SECRET || (() => {
+        throw new Error('REFRESH_TOKEN_EXPIRATION_SECRET must be defined');
+    })(),
+
+    FRONTEND_URL: process.env.FRONTEND_URL || (() => {
+        throw new Error('FRONTEND_URL must be defined');
+    })(),
     ORIGIN: process.env.ORIGIN || (() => {
         throw new Error('ORIGIN must be defined');
+    })(),
+    MONGO_URI: process.env.MONGO_URI || (() => {
+        throw new Error('MONGO_URI must be defined');
     })(),
 };
--- packages/server/src/types/env.d.ts
+++ packages/server/src/types/env.d.ts
@@ -11,19 +11,19 @@ export interface IEnv {
     ORIGIN: string;
     MONGO_URI: string;
 
-    // Nodemailer
-    EMAIL_USER: string;
-    EMAIL_APP_PASSWORD: string;
-    JWT_RESET_PWD_SECRET: string;
-
-    // Cloudinary
-    CLOUDINARY_URL: string;
-    CLOUDINARY_CLOUD_NAME: string;
-    CLOUDINARY_API_KEY: string;
-    CLOUDINARY_API_SECRET: string;
-    LOGO_URL: string;
-
-    // Helmet
-    FRONTEND_DOMAIN: string;
-    BACKEND_URL: string;
+    // // Nodemailer
+    // EMAIL_USER: string;
+    // EMAIL_APP_PASSWORD: string;
+    // JWT_RESET_PWD_SECRET: string;
+    //
+    // // Cloudinary
+    // CLOUDINARY_URL: string;
+    // CLOUDINARY_CLOUD_NAME: string;
+    // CLOUDINARY_API_KEY: string;
+    // CLOUDINARY_API_SECRET: string;
+    // LOGO_URL: string;
+    //
+    // // Helmet
+    // FRONTEND_DOMAIN: string;
+    // BACKEND_URL: string;
 }
