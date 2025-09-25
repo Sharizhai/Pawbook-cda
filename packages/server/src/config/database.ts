2ee06ba0db571bdcca10d86c3b1cd8d372e1e2d0
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-09-25 16:47:33 +0200

    Create database.ts

---
+++ packages/server/src/config/database.ts
@@ -0,0 +1,15 @@
+import { connect } from "mongoose";
+import { env } from "./env";
+
+const { MONGO_URI } = env;
+
+export const connectDB = async () => {
+    try{
+        const db = await connect(MONGO_URI, {family: 4});
+        console.log(`MongoDB connected: ${db.connection.host}`);
+    }
+    catch(error : any){
+        console.error(`Error connecting to MongoDB: ${error.message}`);
+        process.exit(1);
+    }
+}
