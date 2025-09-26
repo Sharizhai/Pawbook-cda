7676c1a49e39afac54fb1d9b4ef52b304ed3f219
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-09-26 16:27:48 +0200

    Update apiResponseUtils.utils.ts

--- packages/server/src/utils/apiResponseUtils.utils.ts
+++ packages/server/src/utils/apiResponseUtils.utils.ts
@@ -1,5 +1,9 @@
 import { Response } from "express"
 
-export const APIResponse = (response : Response, data : any, message : string, status = 200) => {
-    response.status(status).json({data: data, message: message});
+export const APIResponse = (response: Response, data: any, message: string, status = 200) => {
+    response.status(status).json({
+        success: status >= 200 && status < 300,
+        message,
+        data
+    });
 };
