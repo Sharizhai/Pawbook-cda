f61525e68f9b7e190e439faab60379e0a88278a9
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-09-29 10:02:18 +0200

    Update LanguageDropdown.test.ts

--- packages/client/__tests__/components/LanguageDropdown.test.ts
+++ packages/client/__tests__/components/LanguageDropdown.test.ts
@@ -1,4 +1,4 @@
-import { test, expect, vi } from "vitest";
+import { test, expect } from "vitest";
 import { render, fireEvent } from "@testing-library/svelte";
 import LanguageDropdown from "$components/LanguageDropdown.svelte";
 import { app } from "../../src/stores/stores.svelte";
