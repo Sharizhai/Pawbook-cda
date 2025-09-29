d1e89ef6c242cddb23df9cdcda856629fe72f3ec
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-09-29 10:22:02 +0200

    Update userServices.svelte.ts

--- packages/client/src/services/userServices.svelte.ts
+++ packages/client/src/services/userServices.svelte.ts
@@ -2,11 +2,14 @@ import type {UserInformations} from "$types/userTypes";
 import {apiFetch} from "$services/backendServices.svelte";
 
 export async function fetchUserInformations(): Promise<UserInformations> {
-    const response = await apiFetch("/auth/me");
+    const response = await apiFetch("/auth/me",{
+        method: "GET",
+        checkCredentials: true,
+    });
 
-    if(!response.ok) return {};
+    if(!response.ok) return {} as UserInformations;
 
     const responseData = await response.json();
 
-    return responseData.data || {};
+    return responseData.data || {} as UserInformations;
 }
