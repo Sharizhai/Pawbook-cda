6809bef6228b83185664d823245d36dbe81b9ac9
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-10-03 17:24:23 +0200

    Create GetAllPostsUseCase.test.ts

---
+++ packages/server/src/__tests__/useCases/posts/GetAllPostsUseCase.test.ts
@@ -0,0 +1,43 @@
+import {describe, it, expect} from "vitest";
+
+describe("Usecase: We must be able to get all posts", () => {
+    it("should return an empty array of likes if no like are found", async () => {
+
+    });
+
+    it("should return posts with populated user data (id, profilePicture, name & firstName)", async () => {
+
+    });
+
+    it("should return posts sorted by newest first", async () => {
+
+    });
+
+    it("should return an array of posts if some are found", async () => {
+
+    });
+
+    it("should paginate posts with a specified limit", async () => {
+
+    });
+
+    it("should return hasMore: true when more posts exist", async () => {
+
+    });
+
+    it("should return hasMore: false when no more posts exist", async () => {
+
+    });
+
+    it("should include likesCount and commentsCount for each post", async () => {
+
+    });
+
+    it("should return an empty array of comments if no comment are found", async () => {
+
+    });
+
+    it("should return populated comments with textContent and populated author data (id, profilePicture, name & firstName)", async () => {
+
+    });
+});
