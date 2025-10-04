4cf87aec581ea9041a169872ab62d3b433c88220
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-10-04 23:59:30 +0200

    Update stores.svelte.ts

--- packages/client/src/stores/stores.svelte.ts
+++ packages/client/src/stores/stores.svelte.ts
@@ -1,6 +1,9 @@
 import {createUserSlice} from "./userStore.svelte";
 import { createAppSlice } from "./appStore.svelte";
+import {createPostSlice} from "$stores/postStore.svelte";
 
 export const app = createAppSlice();
 
 export const user = createUserSlice();
+
+export const post = createPostSlice();
