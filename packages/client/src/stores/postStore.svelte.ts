411f5029704a2c214def6d41f446c6445a551356
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-10-05 22:12:37 +0200

    Update postStore.svelte.ts

--- packages/client/src/stores/postStore.svelte.ts
+++ packages/client/src/stores/postStore.svelte.ts
@@ -5,7 +5,27 @@ export function createPostSlice() {
     let posts: PostInformations[] = $state([]);
     let information: PostInformations = $state( {} as PostInformations);
 
+    let hasMore = $state(true);
+    let currentPage = $state(1);
+    let isLoading = $state(false);
+
     let isUpdated = $derived(post.information.updated);
+
+    function setPosts(newPosts: PostInformations[]) {
+        posts = newPosts;
+    }
+
+    function addPosts(newPosts: PostInformations[]) {
+        posts = [...posts, ...newPosts];
+    }
+
+    function setHasMore(value: boolean) {
+        hasMore = value;
+    }
+
+    function setCurrentPage(value: number) {
+        currentPage = value;
+    }
 
     return {
         get posts() {
@@ -14,8 +34,19 @@ export function createPostSlice() {
         get information() {
             return information;
         },
+        get hasMore() {
+            return hasMore;
+        },
+        get currentPage() {
+            return currentPage;
+        },
         get isUpdated() {
             return isUpdated;
         },
+
+        setPosts,
+        addPosts,
+        setHasMore,
+        setCurrentPage,
     }
 }
