c549abfe99b97453db53da885d9568ac878da798
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-10-06 22:14:12 +0200

    Update CreateUserUseCase.test.ts

--- packages/server/src/__tests__/useCases/users/CreateUserUseCase.test.ts
+++ packages/server/src/__tests__/useCases/users/CreateUserUseCase.test.ts
@@ -1,22 +1,26 @@
-import {beforeAll, describe, it, expect} from "vitest";
+import {CreateUserUseCase} from "$application/use-cases/user/CreateUserUseCase";
+import {IPasswordServices} from "$domain/interfaces/passwordServices.interface";
 import {IUserRepository} from "$domain/interfaces/userRepository.interface";
-import {Container} from "$types/container";
+import {UserCreationDto} from "$presentation/dto/validation";
+import {beforeAll, describe, it, expect} from "vitest";
 import container from "$config/dependencyInjection";
-import {DateHelper} from "$utils/dateUtils.utils";
 import {UnitUser} from "../../seeds/unit-user";
+import {Container} from "$types/container";
 
 describe("Usecase : We should be able to create a new user", () => {
-    let userRepository: IUserRepository;
     let createUserUseCase: CreateUserUseCase;
+    let passwordServices: IPasswordServices;
+    let userRepository: IUserRepository;
     let validUserData: UserCreationDto;
 
     beforeAll(async () => {
         const c: Container = container;
         userRepository = c.resolve<IUserRepository>("userRepository");
+        passwordServices = c.resolve<IPasswordServices>("argon2Services");
 
         await userRepository.save(UnitUser.jane);
 
-        createUserUseCase = new CreateUserUseCase(userRepository);
+        createUserUseCase = new CreateUserUseCase(userRepository, passwordServices);
 
         validUserData = {
             name: "Doe",
@@ -24,15 +28,8 @@ describe("Usecase : We should be able to create a new user", () => {
             email: "john.doe@johndoe.com",
             password: "Password!123",
             role: "USER",
-            posts: [],
-            animals: [],
-            follows: [],
-            followers: [],
-            createdAt: DateHelper.daysAgo(30),
-            updatedAt: "",
             profileDescription: "Salut, moi c'est John !",
             profilePicture: "",
-            refreshToken: "",
         }
     })
 
@@ -44,10 +41,7 @@ describe("Usecase : We should be able to create a new user", () => {
             password: "Password!123",
         } as UserCreationDto;
 
-        const result = await createUserUseCase.execute(invalidData);
-
-        expect(result.success).toBe(false);
-        expect(result.message).toBe("Le prénom est requis");
+        await expect(createUserUseCase.execute(invalidData)).rejects.toThrow("Le prénom est requis");
     });
 
     it("Should return an error if name is missing", async () => {
@@ -58,10 +52,7 @@ describe("Usecase : We should be able to create a new user", () => {
             password: "Password!123",
         } as UserCreationDto;
 
-        const result = await createUserUseCase.execute(invalidData);
-
-        expect(result.success).toBe(false);
-        expect(result.message).toBe("Le nom est requis");
+        await expect(createUserUseCase.execute(invalidData)).rejects.toThrow("Le nom est requis");
     });
 
     it("Should return an error if email is invalid", async () => {
@@ -72,80 +63,62 @@ describe("Usecase : We should be able to create a new user", () => {
             password: "Password!123",
         } as UserCreationDto;
 
-        const result = await createUserUseCase.execute(invalidData);
-
-        expect(result.success).toBe(false);
-        expect(result.message).toBe("Adresse e-mail invalide");
+        await expect(createUserUseCase.execute(invalidData)).rejects.toThrow("Adresse e-mail invalide");
     });
 
     it("Should return an error if password is too short", async () => {
         const invalidData = {
             firstName: "John",
             name: "Doe",
-            email: "invalid-email",
+            email: "john.doe@test.com",
             password: "Pass!123",
         } as UserCreationDto;
 
-        const result = await createUserUseCase.execute(invalidData);
-
-        expect(result.success).toBe(false);
-        expect(result.message).toBe("Le mot de passe doit faire au moins 12 caractères");
+        await expect(createUserUseCase.execute(invalidData)).rejects.toThrow("Le mot de passe doit faire au moins 12 caractères");
     });
 
     it("Should return an error if password doesn't have an uppercase letter", async () => {
         const invalidData = {
             firstName: "John",
             name: "Doe",
-            email: "invalid-email",
+            email: "john.doe@test.com",
             password: "password!123",
         } as UserCreationDto;
 
-        const result = await createUserUseCase.execute(invalidData);
-
-        expect(result.success).toBe(false);
-        expect(result.message).toBe("Le mot de passe doit contenir au moins une majuscule");
+        await expect(createUserUseCase.execute(invalidData)).rejects.toThrow("Le mot de passe doit contenir au moins une majuscule");
     });
 
     it("Should return an error if password doesn't have an lowercase letter", async () => {
         const invalidData = {
             firstName: "John",
             name: "Doe",
-            email: "invalid-email",
+            email: "john.doe@test.com",
             password: "PASSWORD!123",
         } as UserCreationDto;
 
-        const result = await createUserUseCase.execute(invalidData);
-
-        expect(result.success).toBe(false);
-        expect(result.message).toBe("Le mot de passe doit contenir au moins une minuscule");
+        await expect(createUserUseCase.execute(invalidData)).rejects.toThrow("Le mot de passe doit contenir au moins une minuscule");
     });
 
     it("Should return an error if password doesn't have a number", async () => {
         const invalidData = {
             firstName: "John",
             name: "Doe",
-            email: "invalid-email",
+            email: "john.doe@test.com",
             password: "password!pass",
         } as UserCreationDto;
 
-        const result = await createUserUseCase.execute(invalidData);
-
-        expect(result.success).toBe(false);
-        expect(result.message).toBe("Le mot de passe doit contenir au moins un chiffre");
+        await expect(createUserUseCase.execute(invalidData)).rejects.toThrow("Le mot de passe doit contenir au moins un chiffre");
     });
 
     it("Should return an error if password doesn't have a special character", async () => {
         const invalidData = {
             firstName: "John",
             name: "Doe",
-            email: "invalid-email",
+            email: "john.doe@test.com",
             password: "passwordi123",
         } as UserCreationDto;
 
-        const result = await createUserUseCase.execute(invalidData);
-
-        expect(result.success).toBe(false);
-        expect(result.message).toBe("Le mot de passe doit contenir au moins un caractère spécial");
+        await expect(createUserUseCase.execute(invalidData)).rejects.toThrow("Le mot de passe doit contenir au moins un caractère spécial");
     });
 
     it("Should return an error if email already exist", async () => {
@@ -156,10 +129,7 @@ describe("Usecase : We should be able to create a new user", () => {
             password: "Password!123",
         } as UserCreationDto;
 
-        const result = await createUserUseCase.execute(invalidData);
-
-        expect(result.success).toBe(false);
-        expect(result.message).toBe("Cette adresse email n'est pas autorisée");
+        await expect(createUserUseCase.execute(invalidData)).rejects.toThrow("Cette adresse email existe déjà");
     });
 
     it("Should trim and normalize user data before saving", async () => {
@@ -172,38 +142,42 @@ describe("Usecase : We should be able to create a new user", () => {
 
         const result = await createUserUseCase.execute(dataWithSpaces);
 
-        expect(result.success).toBe(true);
-        expect(result.data?.firstName).toBe("John");
-        expect(result.data?.name).toBe("Doe");
-        expect(result.data?.email).toBe("john.doe@test.com");
+        expect(result.firstName).toBe("John");
+        expect(result.name).toBe("Doe");
+        expect(result.email).toBe("john.doe@test.com");
     });
 
     it("Should lowercase the email before saving", async () => {
-        const data = { ...validUserData, email: "John.Doe@Test.com" };
+        const data = {
+            ...validUserData,
+            email: "Uppercase.Test@Example.com" // Email unique
+        };
         const result = await createUserUseCase.execute(data);
 
-        expect(result.data?.email).toBe("john.doe@test.com");
+        expect(result.email).toBe("uppercase.test@example.com");
     });
 
     it("Should create a new user with valid datas", async () => {
-        const result = await createUserUseCase.execute(validUserData);
+        const user = await createUserUseCase.execute(validUserData);
 
-        expect(result.success).toBe(true);
-        expect(result.data).toBeDefined();
-        expect(result.data?.id).toBeDefined();
-        expect(result.data?.email).toBe("john.doe@test.com");
-        expect(result.data?.firstName).toBe("John");
-        expect(result.data?.name).toBe("Doe");
-        expect(result.data?.password).not.toBe("Password!123");
-        expect(result.data?.role).toBe("USER");
-        expect(result.data?.createdAt).toBeDefined();
+        expect(user).toBeDefined();
+        expect(user.name).toBe("Doe");
+        expect(user.firstName).toBe("John");
+        expect(user.email).toBe("john.doe@johndoe.com");
+        expect(user.role).toBe("USER");
+        expect(user.profileDescription).toBe("Salut, moi c'est John !");
+        expect(user.profilePicture).toBe("");
     });
 
     it("Should not return password in user data", async () => {
-        const result = await createUserUseCase.execute(validUserData);
+        const uniqueData = {
+            ...validUserData,
+            email: "unique@test.com" // Email unique
+        };
 
-        expect(result.success).toBe(true);
-        const userJson = result.data;
-        expect(userJson).not.toHaveProperty('password');
+        const result = await createUserUseCase.execute(uniqueData);
+        const userJson = result.toJSON ? result.toJSON() : result;
+
+        expect(userJson).not.toHaveProperty("password");
     });
 })
