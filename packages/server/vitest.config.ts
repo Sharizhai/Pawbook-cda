268517e7122988fd1ab1178bbb2521ad546b6f91
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-10-18 18:57:04 +0200

    Update vitest.config.ts

--- packages/server/vitest.config.ts
+++ packages/server/vitest.config.ts
@@ -2,33 +2,33 @@ import { defineConfig } from "vitest/config";
 import path from "path";
 
 export default defineConfig(({ mode }) =>({
-  test: {
-    globals: true,
-    environment: 'node',
-    include: [
-      "src/__tests__/**/*.{test,spec}.{ts,js}"
-    ],
-    coverage: {
-      reporter: ["text", "json", "html"],
-      provider: 'istanbul',
-      exclude: [
-        'node_modules/**',
-        '**/*.d.ts',
-      ]
+    test: {
+        globals: true,
+        environment: 'node',
+        include: [
+            "src/__tests__/**/*.{test,spec}.{ts,js}"
+        ],
+        coverage: {
+            reporter: ["text", "json", "html"],
+            provider: 'istanbul',
+            exclude: [
+                'node_modules/**',
+                '**/*.d.ts',
+            ]
+        },
+        watch: false,
+        setupFiles: ["./setup-tests.ts"],
     },
-    watch: false,
-    setupFiles: ["./setup-tests.ts"],
-  },
-  resolve: {
-      alias: {
-        $application: path.resolve("./src/application"),
-        $config: path.resolve("./src/config"),
-        $domain: path.resolve("./src/domain"),
-        $infrastructure: path.resolve("./src/infrastructure"),
-        $presentation: path.resolve("./src/presentation"),
-        $types: path.resolve("./src/types"),
-        $utils: path.resolve("./src/utils"),
-      },
-      conditions: mode === 'test' ? ['browser'] : [],
+    resolve: {
+        alias: {
+            $application: path.resolve(process.cwd(), "packages/server/src/application"),
+            $config: path.resolve(process.cwd(), "packages/server/src/config"),
+            $domain: path.resolve(process.cwd(), "packages/server/src/domain"),
+            $infrastructure: path.resolve(process.cwd(), "packages/server/src/infrastructure"),
+            $presentation: path.resolve(process.cwd(), "packages/server/src/presentation"),
+            $types: path.resolve(process.cwd(), "packages/server/src/types"),
+            $utils: path.resolve(process.cwd(), "packages/server/src/utils"),
+        },
+        conditions: mode === 'test' ? ['browser'] : [],
     }
 }));
