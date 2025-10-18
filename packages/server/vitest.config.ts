0e34efe7c7a54647c567845bd0b85296b128e78b
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-10-18 21:24:30 +0200

    rollback

---
+++ packages/server/vitest.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vitest/config";
+import path from "path";
+
+export default defineConfig(({ mode }) =>({
+    test: {
+        globals: true,
+        environment: 'node',
+        include: [
+            "src/__tests__/**/*.{test,spec}.{ts,js}"
+        ],
+        coverage: {
+            reporter: ["text", "json", "html"],
+            provider: 'istanbul',
+            exclude: [
+                'node_modules/**',
+                '**/*.d.ts',
+            ]
+        },
+        watch: false,
+        setupFiles: ["./setup-tests.ts"],
+    },
+    resolve: {
+        alias: {
+            $application: path.resolve("./src/application"),
+            $config: path.resolve("./src/config"),
+            $domain: path.resolve("./src/domain"),
+            $infrastructure: path.resolve("./src/infrastructure"),
+            $presentation: path.resolve("./src/presentation"),
+            $types: path.resolve("./src/types"),
+            $utils: path.resolve("./src/utils"),
+        },
+        conditions: mode === 'test' ? ['browser'] : [],
+    }
+}));
