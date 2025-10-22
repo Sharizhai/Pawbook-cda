b29431ab2873dcaad6da2afe0b82f40121e7d323
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-10-22 08:36:00 +0200

    install cookie parser & add it to server.ts file

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,5 +1,6 @@
 import {setupRoutes} from "$presentation/routes";
 import {connectDB} from "$config/database";
+import cookieParser from "cookie-parser";
 import { env } from "$config/env";
 import express from "express";
 import cors from "cors";
@@ -14,6 +15,7 @@ app.use(cors({
   methods: ["GET", "PUT", "POST", "DELETE", "PATCH"],
 }));
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
