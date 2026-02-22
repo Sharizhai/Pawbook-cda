5a11adad14046e020589d5f228e2986eac7c3d12
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2026-02-22 22:39:47 +0100

    Update prisma.ts

--- packages/server/src/config/prisma.ts
+++ packages/server/src/config/prisma.ts
@@ -9,9 +9,13 @@ const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-});
+export const prisma = globalForPrisma.prisma ?? (
+    process.env.NODE_ENV === 'test'
+        ? ({} as PrismaClient)
+        : new PrismaClient({
+            log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+          })
+);
 
 if (process.env.NODE_ENV !== 'production') {
     globalForPrisma.prisma = prisma;
