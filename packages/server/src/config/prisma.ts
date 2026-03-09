24f23d486b8cd164f8ee66cf42010f730202b89e
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2026-03-09 19:59:18 +0100

    Update Prisma package + reconfigure prisma

--- packages/server/src/config/prisma.ts
+++ packages/server/src/config/prisma.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 /**
  * Prisma Client Singleton
@@ -9,10 +10,13 @@ const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
 };
 
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
 export const prisma = globalForPrisma.prisma ?? (
     process.env.NODE_ENV === 'test'
         ? ({} as PrismaClient)
         : new PrismaClient({
+            adapter,
             log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
           })
 );
